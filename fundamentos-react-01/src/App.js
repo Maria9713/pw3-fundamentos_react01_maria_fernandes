f1242706e68fc60a7678b322f48c254ad2868730
Maria9713 <mvtfernandes2outlook.com>
2024-03-23 23:13:43 -0300

    Estrutura Base do Projeto

---
+++ fundamentos-react-01/src/App.js
@@ -0,0 +1,14 @@
+import dadosJSON from "./dados.json";
+import Livro from './components/livro';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Livro dadosJSON={dadosJSON}/>
+    </div>
+  );
+}
+
+export default App;
