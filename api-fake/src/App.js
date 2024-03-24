bcbcd33dd2d79bed40cadb63e4a165a37124d5bf
Maria9713 <mvtfernandes2outlook.com>
2024-03-24 17:25:51 -0300

    API FAKE

---
+++ api-fake/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import User from './components/Users/Users.js';
+import CreateUser from './components/CreateUser.js/CreateUser.js';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <CreateUser></CreateUser>
+    </div>
+  );
+}
+
+export default App;
